22115855bbd0b41cb7b07cc9a410b61cc03cb44d
M. Henry Linder <mhlinder@gmail.com>
2016-11-01 22:17:56 -0400

    reorganizes, updates data, constructs a day-of-week means model

--- eda.R
+++ eda.R
@@ -2,35 +2,36 @@
 library(magrittr)
 library(dplyr)
 
+library(lubridate)
+
 library(rstan)
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
 ## rootdir <- "/home/mlinder/research/fitbit"
 rootdir <- "/Users/henry/Dropbox/Programming/active/fitbit"
-load(file.path(rootdir, "data/2016-10-30.Rdata"))
+load(file.path(rootdir, "data/2016-11-01.Rdata"))
 
-steps <- out$daily$steps
-T <- nrow(steps)
+steps <-
+    out$daily$steps %>%
+    mutate(dow = as.factor(wday(steps$time)))
 
-fit1 <- arima(steps$steps, c(1, 0, 0))
+indata <- list(N = nrow(steps),
+               y = steps$steps,
+               k = length(levels(steps$dow)),
+               X = model.matrix(~ dow, data = steps))
 
-indata <- list(T = nrow(steps)-1,
-               x0 = steps$steps[1],
-               x = steps$steps[-1])
-
-fit2 <- stan(file = file.path(rootdir, "ar1.stan"),
+fit2 <- stan(file = file.path(rootdir, "anova.stan"),
              data = indata,
-             iter = 5000,
-             chains = 1)
+             chains = 1, iter = 5000)
 
-plot(indata$x, type = "b", main = "An AR(1) is not a good model for this data")
-steps$fitted <- NA
-alpha <- get_posterior_mean(fit2, pars = "alpha")
-phi <- get_posterior_mean(fit2, pars = "phi")
-for (t in 2:T) {
-    steps$fitted[t] <- alpha + phi*steps$steps[t-1]
-}
-points(steps$fitted[-1])
-lines(steps$fitted[-1])
 
+## Posterior probability that tau (effect size for day of week,
+## relative to Sunday) is larger than 0. Under the null hypothesis
+## H0: tau[2]=...=tau[k]=0, this should be .5
+draws <- extract(fit2)
+(draws$tau[,-1] > 0) %>% colMeans
+
+## But does it contradict an ANOVA?
+lm(steps ~ dow, steps) %>%
+    summary
